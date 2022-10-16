5dfcc3b3f40f9b391dc39fa8a737113803058010
Angel Paulino <apaulino002@gmail.com>
2022-10-16 16:21:24 -0400

    Add amount parameter to producer

--- source/RabbitMQTest.Producer/Program.cs
+++ source/RabbitMQTest.Producer/Program.cs
@@ -4,11 +4,13 @@ using RabbitMQTest.Shared.Services;
 var app = DefaultApp.Create(builder => { });
 
 
-app.MapGet("/", (IRabbitMQService rabbitService) => {
+app.MapGet("/{amount}", (int? amount, IRabbitMQService rabbitService) => {
 
     var messages = new List<Message>();
 
-    for (int i = 1; i <= 30; i++) {
+    var count = (amount is null || amount == 0) ? 20 : amount;
+
+    for (int i = 1; i <= count; i++) {
         messages.Add(new Message() { Id = i, Title = "Message " + i, Content = "Content " + i });
     }
 
